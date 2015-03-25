3bb3234db70292b15c29859e93a9b9f9a180ecee
Justin Ethier <justin.ethier@gmail.com>
2015-03-25 14:41:55 -0400

    TODO stubs for char>?

--- trans.scm
+++ trans.scm
@@ -55,7 +55,18 @@
   (define *Cyc-version-banner* ,*version-banner*)
   ;; TODO: The whitespace characters are space, tab, line feed, form feed (not in parser yet), and carriage return.
   (define call-with-current-continuation call/cc)
-  ; TODO: (define (char>? c1 c2 . cs)
+; TODO:  (define (Cyc-bool-op cmp x lst)
+; TODO:    (cond
+; TODO:      ((null? lst) #t)
+; TODO:      ((cmp x (car lst))
+; TODO:       (Cyc-bool-op (car lst) (cdr lst)))
+; TODO:      (else #f)))
+; TODO:  (define (char>? c1 c2 . cs)
+; TODO:    (Cyc-bool-op 
+; TODO:      (lambda (x y)
+; TODO:       (> (char->integer x) (char->integer y)))
+; TODO:      c1
+; TODO:      (cons c2 cs)))
   (define (char-whitespace? c) (member c '(#\tab #\space #\return #\newline)))
   (define (char-numeric? c) (member c '(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)))
   ; TODO: implement in terms of char>? procs ==> (define (char-upper-case? c)
