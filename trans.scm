d722800131228ed6dbb52313a88b882374b9bfa0
Justin Ethier <justin.ethier@gmail.com>
2015-03-24 22:57:11 -0400

    Added TODO's and notes

--- trans.scm
+++ trans.scm
@@ -55,8 +55,10 @@
   (define *Cyc-version-banner* ,*version-banner*)
   ;; TODO: The whitespace characters are space, tab, line feed, form feed (not in parser yet), and carriage return.
   (define call-with-current-continuation call/cc)
+  ; TODO: (define (char>? c1 c2 . cs)
   (define (char-whitespace? c) (member c '(#\tab #\space #\return #\newline)))
   (define (char-numeric? c) (member c '(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9)))
+  ; TODO: implement in terms of char>? procs ==> (define (char-upper-case? c)
   (define (foldl func accum lst)
     (if (null? lst)
       accum
