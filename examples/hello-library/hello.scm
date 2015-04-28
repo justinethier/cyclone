b5d06c5913035b1472860f13252d5d6fa1858c6a
Justin Ethier <justin.ethier@gmail.com>
2015-04-28 13:57:52 -0400

    Simplify

--- examples/hello-library/hello.scm
+++ examples/hello-library/hello.scm
@@ -1,15 +1,7 @@
-;;;
-;;; Justin Ethier
-;;; husk scheme
-;;;
-;;; A sample program demonstrating how to use libraries.
-;;; To run, go to the directory containing this file and
-;;; execute it using huski:
-;;;
-;;; > huski hello.scm
-;;;
-(import (scheme base)
+(import ;(scheme base)
         (libs lib2)
-        (rename (prefix (libs lib1) test-)))
+        ;(rename (prefix (libs lib1) test-))
+        )
 
-(test-lib1-hello)
+;(test-lib1-hello)
+(write lib2-hello)
--- examples/hello-library/libs/lib2.sld
+++ examples/hello-library/libs/lib2.sld
@@ -1,11 +1,5 @@
-;;;
-;;; Justin Ethier
-;;; husk scheme
-;;;
-;;; A sample library
-;;;
 (define-library (libs lib2)
-    (import (scheme base))
+    ;(import (scheme base))
     (export lib2-hello)
     (begin
         (define lib2-hello 
