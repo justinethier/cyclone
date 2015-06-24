5a4cfea75c0efc6cc01389b3ad6ade5ac0487348
Justin Ethier <justin.ethier@gmail.com>
2015-06-23 21:25:50 -0400

    Debug

--- test2.scm
+++ test2.scm
@@ -7,9 +7,17 @@
 ;Error: Unable to evaluate:  ((procedure () ...) <port>)
 
 ; TODO: need to get this working in compiler, then try interpreter:
-(with-output-to-file 
-  "test.out" 
-  (lambda ()
-    (write 'hello)
-    (display " ")
-    (display 'world)))
+;(with-output-to-file 
+;  "test.out" 
+;  (lambda ()
+;    (write 'hello)
+;    (display " ")
+;    (display 'world)))
+
+; BEGIN test code - trying to get definition of with-output-to-file to work
+(define old (current-output-port))
+(define new (current-output-port '<param-convert> (open-output-file "test.txt")))
+(current-output-port '<param-set!> new)
+(write 'test (current-output-port))
+;(write 'hello-world)
+; END test code
