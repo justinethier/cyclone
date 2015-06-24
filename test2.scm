e4fd8b33870bec821137b57b2c5500f08fe3956e
Justin Ethier <justin.ethier@gmail.com>
2015-06-23 21:12:04 -0400

    Added TODO's

--- test2.scm
+++ test2.scm
@@ -2,6 +2,11 @@
         (scheme file)
         (scheme write))
 
+; TODO: I think this compiles OK (test), but interpreter does not like it:
+;cyclone> ( call-with-output-file "test.txt" (lambda () #f))
+;Error: Unable to evaluate:  ((procedure () ...) <port>)
+
+; TODO: need to get this working in compiler, then try interpreter:
 (with-output-to-file 
   "test.out" 
   (lambda ()
