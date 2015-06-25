83e0a6e662de04f8eba48226879715ddd1493640
Justin Ethier <justin.ethier@gmail.com>
2015-06-25 00:26:25 -0400

    Compiler error when cond does not have else

--- test2.scm
+++ test2.scm
@@ -1,5 +1,12 @@
 (import (scheme base)
         (scheme file)
         (scheme write))
-#t
+
+;; TODO: fails with an "unspecified" error unless there is an else clause. WTF?
+;(write 
+  (cond
+    (#f #f)
+    ;(else #t)
+  )
+;)
 
