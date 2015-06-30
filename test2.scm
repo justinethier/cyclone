e19ad9230f70d19914ca5e640617774d7a681526
Justin Ethier <justin.ethier@gmail.com>
2015-06-29 20:50:45 -0400

    WIP

--- test2.scm
+++ test2.scm
@@ -11,4 +11,10 @@
 (write (when (lambda () #t) 'true))
 (write (when (lambda () #f) 'false))
 
+; Lambda application is broken with pure varargs
+; TODO: once this works, add it to test suite!!!
 ((lambda test (write test)) 1 2 3 4)
+
+;; This is OK
+;(define test (lambda args args))
+;(write (test 1 2 3 4))
