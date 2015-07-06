6c20402e3a741f6561752604813bc608206d0e75
Justin Ethier <justin.ethier@gmail.com>
2015-07-05 22:58:09 -0400

    Need to figure out how to compile cgen with Cyc_procedure

--- test2.scm
+++ test2.scm
@@ -2,20 +2,21 @@
         (scheme file)
         (scheme write))
 
-;; TODO: C macros for funcall1, etc are not being generated even though entries are set
-;;    in the vector. must be another problem inspecting the vector ???
-(write 'hello)
-(let loop ((i 10))
-  (if (zero? i)
-    (write 'done)
-    (loop (- i 1))))
-
-
-(write (command-line-arguments))
-
-(write (when (lambda () #t) 'true))
-(write (when (lambda () #f) 'false))
-
+(write 'Cyc_procedure)
+;;; TODO: C macros for funcall1, etc are not being generated even though entries are set
+;;;    in the vector. must be another problem inspecting the vector ???
+;(write 'hello)
+;(let loop ((i 10))
+;  (if (zero? i)
+;    (write 'done)
+;    (loop (- i 1))))
+;
+;
+;(write (command-line-arguments))
+;
+;(write (when (lambda () #t) 'true))
+;(write (when (lambda () #f) 'false))
+;
 ;; Need to fix this up at some point:
 ;;
 ;; Lambda application is broken with pure varargs
