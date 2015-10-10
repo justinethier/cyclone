b7d13e6899c934197ced4d27c13e09a3117149da
Justin Ethier <justin.ethier@gmail.com>
2015-10-10 02:21:10 -0400

    WIP

--- include/cyclone/types.h
+++ include/cyclone/types.h
@@ -186,12 +186,13 @@ typedef struct {gc_header_type hdr; tag_type tag; double value;} double_type;
 
 /* Define string type */
 typedef struct {gc_header_type hdr; tag_type tag; int len; char *str;} string_type;
-// TODO: new way to allocate strings, but this requires changes to 
-// all functions that allocate strings, the GC, cgen, and maybe more.
-#define make_string(cs, len, s) string_type cs; \
-{ cs.tag = string_tag; cs.len = len; \
-  cs.str = alloca(sizeof(char) * (len + 1)); \
-  strcpy(cs.str, s);}
+//// TODO: new way to allocate strings, but this requires changes to 
+//// all functions that allocate strings, the GC, cgen, and maybe more.
+//// Because these strings are (at least for now) allocaed on the stack.
+//#define make_string(cs, len, s) string_type cs; \
+//{ cs.tag = string_tag; cs.len = len; \
+//  cs.str = alloca(sizeof(char) * (len + 1)); \
+//  strcpy(cs.str, s);}
 // TODO: all of the dhalloc below needs to go away...
 #define make_string(cv,s) string_type cv; cv.tag = string_tag; \
 { int len = strlen(s); cv.str = dhallocp; \
