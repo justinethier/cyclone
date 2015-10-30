be4fd84d6399b7070b8bd83dda37e39e939013c3
Justin Ethier <justin.ethier@gmail.com>
2015-10-30 18:50:33 -0400

    Changed color values
    Many types are allocated with a zeroed-out header, so making the red color 0 is now consistent with that code.

--- gc.c
+++ gc.c
@@ -438,9 +438,9 @@ PHASE 2 - multi-threaded mutator (IE, more than one stack thread):
 //
 // Note: will need to use atomics and/or locking to access any
 // variables shared between threads
-static int        gc_color_mark = 0; // Black, is swapped during GC
-//static const int  gc_color_grey = 1; // TODO: appears unused, clean up
-static int        gc_color_clear = 2; // White, is swapped during GC
+static int        gc_color_mark = 2; // Black, is swapped during GC
+static int        gc_color_clear = 3; // White, is swapped during GC
+//static const int  gc_color_grey = 4; // TODO: appears unused, clean up
 // unfortunately this had to be split up; const colors are located in types.h
 
 static int gc_status_col;
