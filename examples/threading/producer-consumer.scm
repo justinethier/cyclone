af2e836e169b926fcb86d40a1288964c5f7482c5
Justin Ethier <justin.ethier@gmail.com>
2016-01-15 03:10:14 -0500

    Added stub

---
+++ examples/threading/producer-consumer.scm
@@ -0,0 +1,5 @@
+; TODO:
+; try having multiple producers writing to a queue, and 
+; a consumer reading from it
+; an important consideration is how to let the producers write values, since they would be on their stacks! 
+; ideally do not want the application to have to worry about what value came from what thread...
