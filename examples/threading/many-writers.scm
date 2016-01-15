d0bce4a15e881960a190e0f56b0c8e2097aa3584
Justin Ethier <justin.ethier@gmail.com>
2016-01-14 23:34:19 -0500

    Cleaned up demo

--- examples/threading/many-writers.scm
+++ examples/threading/many-writers.scm
@@ -1,26 +1,16 @@
+;; Simple example demonstrating many threads concurrently writing data to stdout
 (import 
   (scheme base)
   (scheme read)
   (scheme write)
   (srfi 18))
 
-;; should not be necessary, just testing
-;; TODO: noticed gc_move error with mutex... may be a GC issue
-;(define m (make-mutex))
-
 (define (write-forever val)
-;  (mutex-lock! m)
   (write val)
-; (list 1)
-;  (mutex-unlock! m)
   (write-forever val))
 
 (define (make-writer val)
   (lambda () (write-forever val)))
-
-;; Try moving closures to heap prior to using threads
-;; TODO: needed?
-(Cyc-minor-gc)
 
 (thread-start!
   (make-thread
@@ -31,6 +21,5 @@
 (thread-start!
   (make-thread
     (make-writer 'thread-3)))
-;; TODO: when main runs the process crashes, but otherwise it seems stable. WTF?
 ((make-writer 'main))
-(read)
+;(read)
