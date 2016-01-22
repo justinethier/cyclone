4b4c7db062399d93aa92cfd372079f4be32bd23e
Justin Ethier <justin.ethier@gmail.com>
2016-01-22 03:21:27 -0500

    Make API consistent

--- include/cyclone/types.h
+++ include/cyclone/types.h
@@ -429,7 +429,7 @@ void gc_mutator_thread_blocked(gc_thread_data *thd, object cont);
 void gc_mutator_thread_runnable(gc_thread_data *thd, object result);
 #define set_thread_blocked(d, c) \
   gc_mutator_thread_blocked(((gc_thread_data *)d), (r))
-#define return_runnable(d, r) \
+#define return_thread_runnable(d, r) \
   gc_mutator_thread_runnable(((gc_thread_data *)d), (r))
 gc_heap *gc_get_heap();
 int gc_minor(void *data, object low_limit, object high_limit, closure cont, object *args, int num_args);
