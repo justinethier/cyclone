8b2e340564a58cd109ee96289dead3240d403b9b
Justin Ethier <justin.ethier@gmail.com>
2016-01-30 22:35:20 -0500

    Initial file

---
+++ sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# A simple script to automate copying changed files to cyclone-bootstrap.
+# Sometimes this is easier than rebuilding everything from this repo.
+#
+DEST=../cyclone-bootstrap
+for var in "$@"
+do
+  echo "cp $var $DEST/$var"
+  cp $var $DEST/$var
+  case "$var" in
+      *.sld) echo "cp `dirname $var`/`basename $var .sld`.c $DEST/`dirname $var`/`basename $var .sld`.c" ; cp `dirname $var`/`basename $var .sld`.c $DEST/`dirname $var`/`basename $var .sld`.c ;;
+  esac
+done
