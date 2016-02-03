ebd8f72ba449b07a4d0c44062e9137c884644b04
Justin Ethier <justin.ethier@gmail.com>
2016-02-02 21:11:21 -0500

    Noted issue with top-level

--- begin-tests.scm
+++ begin-tests.scm
@@ -1,11 +1,11 @@
 ;; A temporary test file
 (import (scheme base ) (scheme write))
 
-;(
-;(lambda ()
+;; TODO: begin splicing does not work at top-level
+((lambda ()
   (begin
     (define tmp1 1)
     (define tmp2 2))
   
   (write `(,tmp1 ,tmp2))
-;))
+))
