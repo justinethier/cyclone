ddfd09b1c2a23dcc182c1f8f2097441f2733b9b7
Justin Ethier <justin.ethier@gmail.com>
2016-02-04 22:45:14 -0500

    Added back test code now that it works

--- kons.scm
+++ kons.scm
@@ -17,11 +17,11 @@
   (list
     (pare? (kons 1 2)) ; =. #t
     (pare? (cons 1 2)) ; =. #f
-;    (kar (kons 1 2))   ; =. 1
-;    (kdr (kons 1 2))   ; =. 2
-;    (let ((k (kons 1 2)))
-;      (set-kar! k 3)
-;      (kar k)) ;=. 3
+    (kar (kons 1 2))   ; =. 1
+    (kdr (kons 1 2))   ; =. 2
+    (let ((k (kons 1 2)))
+      (set-kar! k 3)
+      (kar k)) ;=. 3
 ))
 
 ;(define <pare> (register-simple-type <pare> #f (quote (x y)))) 
