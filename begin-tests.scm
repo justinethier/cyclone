f47b3be21676419c9140f9b38bc52c07fd976701
Justin Ethier <justin.ethier@gmail.com>
2016-02-05 22:53:45 -0500

    Cleanup

--- begin-tests.scm
+++ begin-tests.scm
@@ -1,11 +1,10 @@
-;; A temporary test file
-(import (scheme base ) (scheme write))
+;; An example of using begin to splice in definitions
+(import (scheme base ) 
+        (scheme write))
 
-;; TODO: begin splicing does not work at top-level
-((lambda ()
-  (begin
-    (define tmp1 1)
-    (define tmp2 2))
-  
-  (write `(,tmp1 ,tmp2))
-))
+(begin
+  (define tmp1 1)
+  (define tmp2 2))
+
+(write `(,tmp1 ,tmp2))
+
