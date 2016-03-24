0b38fa2b3269e0d7f358b73d2f8163960c3c64ea
Justin Ethier <justin.ethier@gmail.com>
2016-03-24 00:16:29 -0400

    Tests pass now

--- bytevector-tests.scm
+++ bytevector-tests.scm
@@ -2,8 +2,8 @@
 
 (write #u8(1 2 3 4 5))
 (write (make-bytevector 2 12)) ; =⇒ #u8(12 12)
-;(write (bytevector 1 3 5 1 3 5)) ; =⇒ #u8(1 3 5 1 3 5)
-;(write (bytevector)) ;=⇒ #u8()
+(write (bytevector 1 3 5 1 3 5)) ; =⇒ #u8(1 3 5 1 3 5)
+(write (bytevector)) ;=⇒ #u8()
 
 (write (bytevector-append
           (make-bytevector 1 1)
@@ -11,10 +11,10 @@
           (make-bytevector 3 3)
 ))
 
-;(write (bytevector-u8-ref '#u8(1 1 2 3 5 8 13 21) 5)) ;=⇒ 8
-;(write
-;  (let ((bv (bytevector 1 2 3 4)))
-;    (bytevector-u8-set! bv 1 3)
-;    bv)
-;) ;=⇒ #u8(1 3 3 4)
+(write (bytevector-u8-ref '#u8(1 1 2 3 5 8 13 21) 5)) ;=⇒ 8
+(write
+  (let ((bv (bytevector 1 2 3 4)))
+    (bytevector-u8-set! bv 1 3)
+    bv)
+) ;=⇒ #u8(1 3 3 4)
 
