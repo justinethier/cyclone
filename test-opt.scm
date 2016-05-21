804fcf950d6f5745c2cf3574715fdeb32e75af44
Justin Ethier <justin.ethier@gmail.com>
2016-05-20 21:04:09 -0400

    Added notes

--- test-opt.scm
+++ test-opt.scm
@@ -1,5 +1,11 @@
 ;; A temporary test file
 
+;; Notes:
+;;Need to be careful about which lambda's we optimize, and ultimately remove
+;;paramters from. As a start, we could try to just optimize cases where
+;;there is an application of an anonymous lambda. those should be safe
+;;to change?
+
 (import (scheme base) (scheme write))
 
 (let ((x 1))
