16242a980e9174edfd0e014af90da08fea2609bb
Justin Ethier <justin.ethier@gmail.com>
2016-09-29 04:03:33 -0400

    Modularizing

--- generate-doc-index.scm
+++ generate-doc-index.scm
@@ -17,13 +17,20 @@
 ;; [`load`](api/scheme/load.md#load)
 ;(define line "docs/api/scheme/base.md:- [`denominator`](#denominator)")
 (define line "docs/api/scheme/process-context.md:- [`get-environment-variables`](#get-environment-variables)")
-(let* ((lis (string->list line))
-       (s-file 5)
-       (e-file (index-of lis #\:))
-       (file (substring line s-file e-file))
-       (s-fnc (+ 1 (index-of lis #\`)))
-       (e-fnc (+ s-fnc (index-of (string->list (substring line (+ 0 s-fnc) (string-length line))) #\`)))
-       (fnc (substring line s-fnc e-fnc)))
- (display
-  (string-append 
-   "[`" fnc "`](" file "#" fnc ")")))
+
+(define (convert-line line)
+  (let* ((lis (string->list line))
+         (s-file 5)
+         (e-file (index-of lis #\:))
+         (file (substring line s-file e-file))
+         (s-fnc (+ 1 (index-of lis #\`)))
+         (e-fnc (+ s-fnc (index-of (string->list (substring line (+ 0 s-fnc) (string-length line))) #\`)))
+         (fnc (substring line s-fnc e-fnc)))
+   (string-append 
+     "[`" fnc "`](" file "#" fnc ")")))
+
+(display (convert-line line))
+;(define (loop)
+; (let ((line (read-line)))
+;  (if (not (eof? line))
+;      (display (convert-line line)))))
