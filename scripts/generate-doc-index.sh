9d5a90b53f1ed7f63aca2859775222da9bdd597e
Justin Ethier <justin.ethier@gmail.com>
2016-09-30 02:02:55 -0400

    WIP

--- scripts/generate-doc-index.sh
+++ scripts/generate-doc-index.sh
@@ -3,6 +3,7 @@
 API=api-index.txt
 TMP=/tmp/api-index.txt
 cyclone scripts/convert-doc-index.scm
+#grep -r "^- \[" docs/api/primitives.md | ./scripts/convert-doc-index > $TMP
 grep -r "^- \[" docs/api/scheme/* | ./scripts/convert-doc-index > $TMP
 grep -r "^- \[" docs/api/srfi/* | ./scripts/convert-doc-index >> $TMP
 sort $TMP > $API
