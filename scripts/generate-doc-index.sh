9bfc5cee3bbb1b60eb6a7c29c956dcc430ea42e1
Justin Ethier <justin.ethier@gmail.com>
2016-10-01 17:49:20 -0400

    Added primitives

--- scripts/generate-doc-index.sh
+++ scripts/generate-doc-index.sh
@@ -4,7 +4,7 @@ API=api-index.txt
 TMP=/tmp/api-index.txt
 cyclone scripts/convert-doc-index.scm
 cyclone scripts/alphabetize.scm
-#grep -r "^- \[" docs/api/primitives.md | ./scripts/convert-doc-index > $TMP
-grep -r "^- \[" docs/api/scheme/* | ./scripts/convert-doc-index > $TMP
+grep "^- \[" docs/api/* | ./scripts/convert-doc-index > $TMP
+grep -r "^- \[" docs/api/scheme/* | ./scripts/convert-doc-index >> $TMP
 grep -r "^- \[" docs/api/srfi/* | ./scripts/convert-doc-index >> $TMP
 sort $TMP | ./scripts/alphabetize > $API
