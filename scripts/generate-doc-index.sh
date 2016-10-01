b4ce67210c11ef13b48601eff71f61093dd69282
Justin Ethier <justin.ethier@gmail.com>
2016-10-01 16:28:06 -0400

    Added index

--- scripts/generate-doc-index.sh
+++ scripts/generate-doc-index.sh
@@ -6,6 +6,5 @@ cyclone scripts/convert-doc-index.scm
 cyclone scripts/alphabetize.scm
 #grep -r "^- \[" docs/api/primitives.md | ./scripts/convert-doc-index > $TMP
 grep -r "^- \[" docs/api/scheme/* | ./scripts/convert-doc-index > $TMP
-grep -r "^- \[" docs/api/scheme/cyclone/* | ./scripts/convert-doc-index > $TMP
 grep -r "^- \[" docs/api/srfi/* | ./scripts/convert-doc-index >> $TMP
 sort $TMP | ./scripts/alphabetize > $API
