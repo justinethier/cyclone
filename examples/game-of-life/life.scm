03416a31f95f17dfa2c742dc3b9d7357fc55ecb4
Justin Ethier <justin.ethier@gmail.com>
2016-10-11 03:19:31 -0400

    Cleanup

--- examples/game-of-life/life.scm
+++ examples/game-of-life/life.scm
@@ -11,25 +11,15 @@
 ;;;  > huski life.scm
 ;;;
 (import (scheme base)
-;        (example life)
-;        (example grid)
-;; TODO:
         (only (example life) life)
         (rename (prefix (example grid) grid-)
-                (grid-make make-grid))
-)
+                (grid-make make-grid)))
 ;; Initialize a grid with a glider.
-;(define grid (make-grid 24 24))
-;(grid-put! grid 1 1 #t)
-;(grid-put! grid 2 2 #t)
-;(grid-put! grid 3 0 #t)
-;(grid-put! grid 3 1 #t)
-;(grid-put! grid 3 2 #t)
-(define grid (make 24 24))
-(put! grid 1 1 #t)
-(put! grid 2 2 #t)
-(put! grid 3 0 #t)
-(put! grid 3 1 #t)
-(put! grid 3 2 #t)
+(define grid (make-grid 24 24))
+(grid-put! grid 1 1 #t)
+(grid-put! grid 2 2 #t)
+(grid-put! grid 3 0 #t)
+(grid-put! grid 3 1 #t)
+(grid-put! grid 3 2 #t)
 ;; Run for x iterations.
 (life grid 80)
