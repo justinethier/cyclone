04b32d6a5e4d8112f25761dcfe7cc2e5d5efee2e
Justin Ethier <justin.ethier@gmail.com>
2017-12-11 13:44:47 -0500

    Tweaked syntax

--- tests/let-syntax.scm
+++ tests/let-syntax.scm
@@ -1,13 +1,16 @@
 (import (scheme base) (scheme write) (scheme cyclone pretty-print))
 
 #;(letrec-syntax
-  ((my-or (syntax-rules
-            ()
+  ((my-or (syntax-rules ()
             ((my-or) #f)
             ((my-or e) e)
             ((my-or e1 e2 ...)
              (let ((temp e1)) (if temp temp (my-or e2 ...)))))))
-  (let ((x #f) (y 7) (temp 8) (let odd?) (if even?))
+  (let ((x #f) 
+        (y 7) 
+        (temp 8) 
+        (let odd?) 
+        (if even?))
     (my-or x (let temp) (if y) y))) ;; ==> 7
 
 (define-syntax my-let
