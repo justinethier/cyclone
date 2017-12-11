7e2a9f46c2187f3b94ac72602ceca4384fd8eb1e
Justin Ethier <justin.ethier@gmail.com>
2017-12-11 12:15:07 -0500

    Added letrec-syntax example from r7rs

--- tests/let-syntax.scm
+++ tests/let-syntax.scm
@@ -1,4 +1,14 @@
-(import (scheme base) (scheme write))
+(import (scheme base) (scheme write) (scheme cyclone pretty-print))
+
+#;(letrec-syntax
+  ((my-or (syntax-rules
+            ()
+            ((my-or) #f)
+            ((my-or e) e)
+            ((my-or e1 e2 ...)
+             (let ((temp e1)) (if temp temp (my-or e2 ...)))))))
+  (let ((x #f) (y 7) (temp 8) (let odd?) (if even?))
+    (my-or x (let temp) (if y) y))) ;; ==> 7
 
 (define-syntax my-let
   (syntax-rules
