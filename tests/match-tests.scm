142dbc3fb4744f3ee87e3052d3f043de261e7481
Justin Ethier <justin.ethier@gmail.com>
2018-02-01 13:38:57 -0500

    Temporarily enable code that generates a compiler error

--- tests/match-tests.scm
+++ tests/match-tests.scm
@@ -95,11 +95,13 @@
   (match
    lst
    ;(() 0)
-   (((? number? n) (or 's 'seconds 'sec) . rest)
-    (+ 0 (* #e1 n) (calc-time rest)))
-;   ;; TODO: interesting compiler error with these lines:
-;   (((? number? n) (or 's 'seconds 'sec) ) ;. rest)
-;    (+ (* #e1 n) )) ;(calc-time rest)))
+;; Temporarily commenting these 2 lines out to track down compiler error with the next:
+;   (((? number? n) (or 's 'seconds 'sec) . rest)
+;    (+ 0 (* #e1 n) (calc-time rest)))
+   ;; TODO: interesting compiler error with these lines:
+   (((? number? n) (or 's 'seconds 'sec) ) ;. rest)
+    (+ (* #e1 n) )) ;(calc-time rest)))
+;;
    (((? number? n) (or 'm 'min 'minutes) . rest)
     (+ (* #e60 n) (calc-time rest)))
    (((? number? n) (or 'hours 'h) . rest)
