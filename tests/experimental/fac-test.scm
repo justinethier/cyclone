657c6c0dadcc0560669cabe862fe9adc3da67e82
Justin Ethier <justin.ethier@gmail.com>
2018-10-03 18:34:09 -0400

    WIP

--- tests/experimental/fac-test.scm
+++ tests/experimental/fac-test.scm
@@ -12,14 +12,60 @@
 (write (fac 10))
 #| Next-gen runtime:
 
-static void __host_lambda_1(void *data, int pc, TBD) { // rest of args TBD, maybe a C array, length, what else?
+static void __host_lambda_1(void *data, int pc, int argc, object *args) { // TODO: self? cont?
+  object top;
+  loop:
+  top = alloca(
+  // TODO: if exceeded stack limit, initiate minor GC
+  //       bundle up args, pc, and pass them along
+
   switch(pc) {
     3: { // Lambda ID 3
+      //static void __lambda_3(void *data, int argc, object self_7312, object r_7310) {
+      //  return_closcall2(data,  __glo_write_scheme_write,  primitive__75halt, r_7310);; 
+      return_closcall2(data,  __glo_write_scheme_write,  primitive__75halt, args[1]); 
+      break;
+    }
+    1: { // Lambda ID 1
+//static void __lambda_1(void *data, int argc, closure _,object k_735, object n_731_732) {
+//  Cyc_st_add(data, "fac-test.scm:fac");
+//  object c_7316 = Cyc_num_fast_eq_op(data,n_731_732, obj_int2obj(0));
+//if( (boolean_f != c_7316) ){ 
+//  return_closcall1(data,  k_735,  obj_int2obj(1));
+//} else { 
+//  
+//closureN_type c_7319;
+//c_7319.hdr.mark = gc_color_red;
+// c_7319.hdr.grayed = 0;
+//c_7319.tag = closureN_tag;
+// c_7319.fn = (function_type)__lambda_2;
+//c_7319.num_args = 1;
+//c_7319.num_elements = 2;
+//c_7319.elements = (object *)alloca(sizeof(object) * 2);
+//c_7319.elements[0] = k_735;
+//c_7319.elements[1] = n_731_732;
+//
+//
+//complex_num_type local_7329; object c_7330 = Cyc_fast_sub(data,&local_7329,n_731_732, obj_int2obj(1));
+//return_closcall2(data,  __glo_fac,  &c_7319, c_7330);}
+//; 
+//}
       // TODO
       break;
     }
-    default: 
+    2: { // Lambda ID 2
+      //static void __lambda_2(void *data, int argc, object self_7311, object r_737) {
+      //complex_num_type local_7324; object c_7325 = Cyc_fast_mul(data,&local_7324,((closureN)self_7311)->elements[1], r_737);
+      //return_closcall1(data,  ((closureN)self_7311)->elements[0],  c_7325);; 
+      object local_7324 = alloca(sizeof(complex_num_type));
+      object c_7325 = Cyc_fast_mul(data,local_7324,((closureN)args[0])->elements[1], args[1]);
+ // TODO: can we be smart enough to call lambda directly, instead of via closure?
+      return_closcall1(data,  ((closureN)args[0])->elements[0],  c_7325); 
+      break;
+    }
+    default: {
       // raise error
+    }
   }
 }
 |#
