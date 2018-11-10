8cbcf821212b5b7493b91a5069a83c1975afbc00
Justin Ethier <justin.ethier@gmail.com>
2018-11-10 06:52:54 -0500

    Added a special (program) feature

--- test-find-local-vars.scm
+++ test-find-local-vars.scm
@@ -1,4 +1,10 @@
-(import (scheme base) (scheme write) (scheme cyclone ast) (scheme cyclone util) (scheme cyclone pretty-print))
+(cond-expand 
+  (program
+    (import (scheme base) 
+            (scheme write) 
+            (scheme cyclone ast) 
+            (scheme cyclone util) 
+            (scheme cyclone pretty-print))))
 
 ;; Local variable reduction:
 ;; Reduce given sexp by replacing certain lambda calls with a let containing
@@ -122,40 +128,43 @@
       (return
         (scan sexp)))))
 
-(define sexp
-             '(lambda
-                (k$1073 i$88$682 first$89$683 row$90$684)
-                (if (Cyc-fast-eq
-                      i$88$682
-                      number-of-cols$68$671)
-                  (k$1073
-                    (Cyc-fast-eq
-                      i$88$682
-                      number-of-cols$68$671))
-                  ((lambda
-                     (k$1080)
-                     (if (Cyc-fast-eq
-                           (car first$89$683)
-                           (car row$90$684))
-                       (k$1080 if-equal$76$674)
-                       (k$1080 if-different$77$675)))
-                   (lambda
-                     (r$1079)
-                     (Cyc-seq
-                       (vector-set!
-                         vec$79$677
-                         i$88$682
-                         r$1079)
-                       ((cell-get lp$80$87$681)
-                        k$1073
-                        (Cyc-fast-plus i$88$682 1)
-                        (cdr first$89$683)
-                        (cdr row$90$684))))))))
-
-;(pretty-print
-;  (ast:ast->pp-sexp
-;    (ast:sexp->ast sexp)))
-
-(pretty-print
-  (ast:ast->pp-sexp
-    (opt:local-var-reduction (ast:sexp->ast sexp))))
+(cond-expand
+  (program
+    (define sexp
+                 '(lambda
+                    (k$1073 i$88$682 first$89$683 row$90$684)
+                    (if (Cyc-fast-eq
+                          i$88$682
+                          number-of-cols$68$671)
+                      (k$1073
+                        (Cyc-fast-eq
+                          i$88$682
+                          number-of-cols$68$671))
+                      ((lambda
+                         (k$1080)
+                         (if (Cyc-fast-eq
+                               (car first$89$683)
+                               (car row$90$684))
+                           (k$1080 if-equal$76$674)
+                           (k$1080 if-different$77$675)))
+                       (lambda
+                         (r$1079)
+                         (Cyc-seq
+                           (vector-set!
+                             vec$79$677
+                             i$88$682
+                             r$1079)
+                           ((cell-get lp$80$87$681)
+                            k$1073
+                            (Cyc-fast-plus i$88$682 1)
+                            (cdr first$89$683)
+                            (cdr row$90$684))))))))
+    
+    ;(pretty-print
+    ;  (ast:ast->pp-sexp
+    ;    (ast:sexp->ast sexp)))
+    
+    (pretty-print
+      (ast:ast->pp-sexp
+        (opt:local-var-reduction (ast:sexp->ast sexp))))
+    ))
