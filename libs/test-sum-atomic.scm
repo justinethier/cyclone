13585948c4d369fd81d748f153fe42384cc6c967
Justin Ethier <justin.ethier@gmail.com>
2019-06-11 18:01:03 -0400

    Tweak to make test run longer

--- libs/test-sum-atomic.scm
+++ libs/test-sum-atomic.scm
@@ -18,7 +18,7 @@
       (sum-loop (- n 1))))
 
 (define (sum-entry-pt)
-  (sum-loop (* 10 10 100 100)))
+  (sum-loop (* 10 100 100 100)))
 
 ;; Thread - Do something, then let main thread know when we are done
 (define t1 (make-thread sum-entry-pt))
@@ -27,12 +27,18 @@
 (define t4 (make-thread sum-entry-pt))
 (define t5 (make-thread sum-entry-pt))
 (define t6 (make-thread sum-entry-pt))
+(define t7 (make-thread sum-entry-pt))
+(define t8 (make-thread sum-entry-pt))
+(define t9 (make-thread sum-entry-pt))
 (thread-start! t1)
 (thread-start! t2)
 (thread-start! t3)
 (thread-start! t4)
 (thread-start! t5)
 (thread-start! t6)
+(thread-start! t7)
+(thread-start! t8)
+(thread-start! t9)
 
 (thread-join! t1)
 (thread-join! t2)
@@ -40,6 +46,9 @@
 (thread-join! t4)
 (thread-join! t5)
 (thread-join! t6)
+(thread-join! t7)
+(thread-join! t8)
+(thread-join! t9)
 (display "main thread done, sum = ")
 (display (ref *sum*))
 (newline)
--- libs/test-sum-mutex.scm
+++ libs/test-sum-mutex.scm
@@ -19,7 +19,7 @@
       (sum-loop (- n 1))))
 
 (define (sum-entry-pt)
-  (sum-loop (* 10 10 100 100)))
+  (sum-loop (* 10 100 100 100)))
 
 ;; Thread - Do something, then let main thread know when we are done
 (define t1 (make-thread sum-entry-pt))
@@ -28,12 +28,18 @@
 (define t4 (make-thread sum-entry-pt))
 (define t5 (make-thread sum-entry-pt))
 (define t6 (make-thread sum-entry-pt))
+(define t7 (make-thread sum-entry-pt))
+(define t8 (make-thread sum-entry-pt))
+(define t9 (make-thread sum-entry-pt))
 (thread-start! t1)
 (thread-start! t2)
 (thread-start! t3)
 (thread-start! t4)
 (thread-start! t5)
 (thread-start! t6)
+(thread-start! t7)
+(thread-start! t8)
+(thread-start! t9)
 
 (thread-join! t1)
 (thread-join! t2)
@@ -41,6 +47,9 @@
 (thread-join! t4)
 (thread-join! t5)
 (thread-join! t6)
+(thread-join! t7)
+(thread-join! t8)
+(thread-join! t9)
 (display "main thread done, sum = ")
 (mutex-lock! m)
 (display *sum*)
