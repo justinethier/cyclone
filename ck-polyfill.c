a461da48cd7121889c8382d2670b523927daf34a
Justin Ethier <justin.ethier@gmail.com>
2021-01-05 19:26:15 -0800

    Added more CAS operations

--- ck-polyfill.c
+++ ck-polyfill.c
@@ -10,15 +10,33 @@
 
 #include "cyclone/types.h"
 #include "cyclone/runtime.h"
-#include <ck_pr.h>
+#include "ck_pr.h"
 #include <unistd.h>
 
 // TODO: global pthread mutex lock for this? obviously not ideal but the
 // whole purpose of this module is a minimal interface for compatibility
 // not speed
 
-bool
-ck_pr_cas_int(int *target, int old_value, int new_value)
+bool ck_pr_cas_int(int *target, int old_value, int new_value)
+{
+  if (*target == old_value) {
+    *target = new_value;
+    return true;
+  }
+  return false;
+}
+
+bool ck_pr_cas_ptr(void *target, void *old_value, void *new_value)
+{
+  if ( *(void **)target == old_value ) {
+    *(void **)target = new_value;
+    return true;
+  }
+  return false;
+  // *(void **)v = set;                                                             
+}
+
+bool ck_pr_cas_8(uint8_t *target, uint8_t old_value, uint8_t new_value)
 {
   if (*target == old_value) {
     *target = new_value;
--- ck_pr.h
+++ ck_pr.h
@@ -3,7 +3,13 @@
 
 #include <stdbool.h>
 
- bool
- ck_pr_cas_int(int *target, int old_value, int new_value);
+bool
+ck_pr_cas_ptr(void *target, void *old_value, void *new_value);
+
+bool
+ck_pr_cas_int(int *target, int old_value, int new_value);
+
+bool
+ck_pr_cas_8(uint8_t *target, uint8_t old_value, uint8_t new_value);
 
 #endif                          /* CYCLONE_CK_POLYFILL_H */
