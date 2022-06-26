265e34ee08447ba51687cf768d31ff8e1ab983f8
Justin Ethier <justin.ethier@gmail.com>
2022-06-26 18:52:16 -0400

    Add header comment

--- tests/c-compiler-options.scm
+++ tests/c-compiler-options.scm
@@ -1,11 +1,9 @@
+;; Simple test to prevent regressions of top-level c-compiler-options 
 (import (scheme base)
         (scheme write)
         (cyclone foreign))
 
-;(c-linker-options "-I/tmp")
 (c-compiler-options "-I/tmp")
-;(define c-compiler-options list)
-;(c-linker-options "-I/tmp")
 
 (display "hello")
 
