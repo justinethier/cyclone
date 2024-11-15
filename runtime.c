955695f554d19e851e7c67a33bb909d7ff7ad60c
Yorick Hardy <yorickhardy@gmail.com>
2024-11-15 23:02:55 +0200

    runtime: force the garbage collector to run when a thread exits
    This is a first attempt to improve the memory usage reported in
    issue #534.

--- runtime.c
+++ runtime.c
@@ -7181,6 +7181,7 @@ void Cyc_exit_thread(void *data, object _, int argc, object * args)
   gc_remove_mutator(thd);
   ck_pr_cas_int((int *)&(thd->thread_state), CYC_THREAD_STATE_RUNNABLE,
                 CYC_THREAD_STATE_TERMINATED);
+  gc_force();
   pthread_exit(NULL);
 }
 
